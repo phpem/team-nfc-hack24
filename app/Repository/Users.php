d1bd6ca25bafce4ba35af34e50d0983afb95f3ec
Antonios Pavlakis <antonios.pavlakis@twist360.com>
2015-05-02 22:37:38 +0100

    Retrieving users and teams using the DB.

--- app/Repository/Users.php
+++ app/Repository/Users.php
@@ -3,38 +3,48 @@
 namespace Teamnfc\Repository;
 
 
+use Illuminate\Database\DatabaseManager;
 use Teamnfc\Entity\EntityFactory;
 use Teamnfc\Entity\TeamEntity;
 use Illuminate\Database\Connection;
 
+
 class Users {
 
+    /**
+     * @var Connection
+     */
     protected $db;
 
-    public function __construct(Connection $db)
+    public function __construct(DatabaseManager $db)
     {
         $this->db = $db;
     }
 
     public function getUsers()
     {
-        return $this->db->table('users')->get();
+        $users =  $this->db->table('users')->get();
+        $entities = [];
+        foreach ($users as $user) {
+            $entities[] = EntityFactory::get('UserEntity', (array)$user);
+        }
+
+        return $entities;
     }
 
     public function getTeamsForUser($user)
     {
-        $dummyData = [
-            ['id' => '1', 'team_name' => 'team-NFC', 'org_id' => '2', 'created_at' => '', 'updated_at' => ''],
-            ['id' => '2', 'team_name' => 'team-NFC', 'org_id' => '2', 'created_at' => '', 'updated_at' => ''],
-            ['id' => '3', 'team_name' => 'team-NFC', 'org_id' => '2', 'created_at' => '', 'updated_at' => ''],
-            ['id' => '4', 'team_name' => 'team-NFC', 'org_id' => '2', 'created_at' => '', 'updated_at' => ''],
-            ['id' => '5', 'team_name' => 'team-NFC', 'org_id' => '2', 'created_at' => '', 'updated_at' => '']
-        ];
-
+        // select t.* from teams AS t INNER JOIN team_users as tu where tu.user_id = 7 AND tu.team_id = t.id
+        $teams = $this->db->table('teams')
+            ->join('team_users', function($join) use ($user)
+            {
+                $join->on('team_users.team_id', '=', 'teams.id')
+                    ->where('team_users.user_id', '=', $user->id);
+            })->get();
 
         $entities = [];
-        foreach ($dummyData as $data) {
-            $entities[] = EntityFactory::get('TeamEntity', $data);
+        foreach ($teams as $data) {
+            $entities[] = EntityFactory::get('TeamEntity', (array)$data);
         }
 
         return $entities;
