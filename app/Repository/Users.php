706025e68e87c7fecfb24cb720029dedc3140e0f
Antonios Pavlakis <antonios.pavlakis@twist360.com>
2015-05-02 22:45:26 +0100

    Using DB to retrieve all data.

--- app/Repository/Users.php
+++ app/Repository/Users.php
@@ -52,19 +52,11 @@ class Users {
 
     public function getUsersByIds($userIDs)
     {
-        $dummyData = [
-            ['id' => '1', 'email' => 'me1@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A1', 'last_name' => 'B1'],
-            ['id' => '2', 'email' => 'me2@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A2', 'last_name' => 'B2'],
-            ['id' => '3', 'email' => 'me3@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A3', 'last_name' => 'B3'],
-            ['id' => '4', 'email' => 'me4@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A4', 'last_name' => 'B4'],
-            ['id' => '5', 'email' => 'me5@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A5', 'last_name' => 'B5'],
-            ['id' => '6', 'email' => 'me6@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A6', 'last_name' => 'B6'],
-        ];
-
+        $users =  $this->db->table('users')->whereIn('id', $userIDs)->get();
 
         $entities = [];
-        foreach ($dummyData as $data) {
-            $entities[] = EntityFactory::get('UserEntity', $data);
+        foreach ($users as $user) {
+            $entities[] = EntityFactory::get('UserEntity', (array)$user);
         }
 
         return $entities;
@@ -72,10 +64,9 @@ class Users {
 
     public function getUserById($userID)
     {
-        $data = [
-            'user_id' => $userID
-        ];
-        return EntityFactory::get('UserEntity', $data);
+        $user =  $this->db->table('users')->find($userID);
+
+        return EntityFactory::get('UserEntity', (array)$user);
     }
 
 }
