0fd30fda4bf96cd4cecaee8ddf878aaff409e347
Antonios Pavlakis <antonios.pavlakis@twist360.com>
2015-05-02 20:22:05 +0100

    Updated with dummy data Users repository.

--- app/Repository/Users.php
+++ app/Repository/Users.php
@@ -29,12 +29,30 @@ class Users {
 
     public function getUsersByIds($userIDs)
     {
-        return [];
+        $dummyData = [
+            ['id' => '1', 'email' => 'me1@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A1', 'last_name' => 'B1'],
+            ['id' => '2', 'email' => 'me2@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A2', 'last_name' => 'B2'],
+            ['id' => '3', 'email' => 'me3@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A3', 'last_name' => 'B3'],
+            ['id' => '4', 'email' => 'me4@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A4', 'last_name' => 'B4'],
+            ['id' => '5', 'email' => 'me5@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A5', 'last_name' => 'B5'],
+            ['id' => '6', 'email' => 'me6@me.com', 'password' => '', 'remember_token' => '', 'created_at' => '', 'first_name' => 'A6', 'last_name' => 'B6'],
+        ];
+
+
+        $entities = [];
+        foreach ($dummyData as $data) {
+            $entities[] = EntityFactory::get('UserEntity', $data);
+        }
+
+        return $entities;
     }
 
     public function getUserById($userID)
     {
-        return [];
+        $data = [
+            'user_id' => $userID
+        ];
+        return EntityFactory::get('UserEntity', $data);
     }
 
 }
