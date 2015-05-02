6d26f7b3877b2a57412f8cd239f12c8b254ed344
Gareth Jones <me@gazj.co.uk>
2015-05-02 15:23:40 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 echo "Hold onto your butts"
 composer install --no-dev --verbose --prefer-dist --optimize-autoloader --no-progress
 npm install
-#bower install
-#bundle install
+bower install
+bundle install
+gulp setup
 gulp --prod
 chmod 777 storage
 chmod 777 storage/logs
