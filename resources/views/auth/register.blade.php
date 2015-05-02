b379d36bbad9321e2a7b57b3871be801c05b23f2
Gareth Jones <me@gazj.co.uk>
2015-05-02 19:11:12 +0100

    Allow user registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,7 +24,14 @@
 						<div class="form-group">
 							<label class="col-md-4 control-label">Name</label>
 							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+								<input type="text" class="form-control" name="first_name" value="{{ old('first_name') }}">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Last Name</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
 							</div>
 						</div>
 
