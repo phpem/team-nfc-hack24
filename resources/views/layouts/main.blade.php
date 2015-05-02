eaffa76412eb864b505b9d1dfa6a94b9a694a5dc
brunty <openblue555@gmail.com>
2015-05-02 17:27:22 +0100

    Splash Page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,13 +1,16 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Team NFC (No fucking clue)</title>
+    <title>@yield('meta-title', 'Team NFC (No fucking clue)')</title>
     <link rel="stylesheet" href="css/main.min.css"/>
 </head>
 <body>
-
     @yield('content')
-
-    <script src="js/main.min.js"></script>
+<script src="js/main.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $(document).foundation();
+        });
+    </script>
 </body>
 </html>
