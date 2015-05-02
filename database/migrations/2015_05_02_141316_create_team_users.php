ed659f67891a81c61d5402cce6400010eb2d328f
Antonios Pavlakis <antonios.pavlakis@twist360.com>
2015-05-02 15:31:58 +0100

    [Database] Added initial DB schema.

---
+++ database/migrations/2015_05_02_141316_create_team_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('team_users', function(Blueprint $table)
+        {
+            $table->integer('team_id');
+            $table->integer('user_id');
+            $table->tinyInteger('is_manager');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
---
+++ database/migrations/2015_05_02_142217_create_table_vote.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableVote extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('votes', function(Blueprint $table)
+        {
+            $table->increments('criteria_id');
+            $table->tinyInteger('score');
+            $table->integer('team_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
