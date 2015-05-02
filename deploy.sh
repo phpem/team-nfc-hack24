f6294541ba37c72683051e2d154b40bfef5eef90
Gareth Jones <me@gazj.co.uk>
2015-05-02 15:58:15 +0100

    Enable db migration and seeding

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ set -e
 echo "Hold onto your butts"
 composer install --no-dev --verbose --prefer-dist --optimize-autoloader --no-progress
 php artisan cache:clear
-#php artisan migrate --no-interaction
+php artisan migrate --no-interaction
+php artisan db:seed --no-interaction
 
 npm install
 bower install --allow-root
