88a90391223ba269bcbb2070c65b29943f6ea19f
brunty <openblue555@gmail.com>
2015-05-02 15:43:29 +0100

    Gulp tasks update

--- gulp/tasks/watchers.js
+++ gulp/tasks/watchers.js
@@ -6,7 +6,7 @@ var config = require('../config');
  $ gulp watch
  */
 gulp.task('watch', function() {
-    gulp.watch([config.paths.assets.css + '**/*.scss', config.paths.assets.css + '**/*.sass'], ['styles']).on('error', function(err) {
+    gulp.watch([config.paths.assets.sass + '**/*.scss', config.paths.assets.sass + '**/*.sass'], ['styles']).on('error', function(err) {
         console.error('Error', err.message);
     });
     gulp.watch(config.paths.assets.js + '**/*.js', ['jshint', 'scripts']).on('error', function(err) {
