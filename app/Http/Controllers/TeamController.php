d89ca06c3e78b6af1e108ac9b42bd367d049b09d
Antonios Pavlakis <antonios.pavlakis@twist360.com>
2015-05-03 02:14:33 +0100

    Updated controllers with new reference to UsersRepository.

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -3,7 +3,7 @@
 namespace Teamnfc\Http\Controllers;
 
 use Illuminate\Contracts\Auth\Authenticatable;
-use Teamnfc\Repository\Users;
+use Teamnfc\Repository\UsersRepository;
 
 /**
  * AccountController
@@ -15,7 +15,7 @@ final class TeamController extends Controller
         return view('team/index');
     }
 
-    public function teams(Authenticatable $user, Users $usersRepository) {
+    public function teams(Authenticatable $user, UsersRepository $usersRepository) {
         $teams = $usersRepository->getTeamsForUser($user);
         return view('account/teams', ['teams'   =>  $teams]);
     }
