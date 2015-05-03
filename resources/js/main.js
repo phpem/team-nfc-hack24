18d8bb2bd20f9838aff12f8a0dedce7a67ee44cd
Gareth Jones <me@gazj.co.uk>
2015-05-03 02:35:29 +0100

    Dirty ajax to change the manager when team is changed

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
     $(document).foundation();
     $('#search').focus();
+
+    $('#team_id').change(function(event) {
+        var teamId = $(this).val();
+        $('#manager_id').html('');
+
+        $.get('/vote/get-manager/' + teamId, function(data){
+            $.each(data, function(index, manager) {
+                $('#manager_id').append('<option value="' + manager.id + '">' + manager.name + '</option>');
+            });
+        });
+    });
 });
