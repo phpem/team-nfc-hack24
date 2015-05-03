144ac5410adc0d987ae75c5b826b93da71144772
Antonios Pavlakis <antonios.pavlakis@twist360.com>
2015-05-03 01:25:53 +0100

    Updated with additional columns.

--- app/Entity/UserEntity.php
+++ app/Entity/UserEntity.php
@@ -12,17 +12,20 @@ class UserEntity extends EntityFactory
     public $password;
     public $remember_token;
     public $created_at;
+    public $updated_at;
     public $first_name;
     public $last_name;
+    public $avatar;
 
-    public function __construct($id = null, $email = null, $password = null, $remember_token = null, $created_at = null,
-        $fist_name = null, $last_name = null)
+    public function __construct($id = null, $email = null, $password = null, $remember_token = null,
+        $created_at = null, $updated_at = null, $fist_name = null, $last_name = null, $avatar = null)
     {
         $this->id           = $id;
         $this->email        = $email;
         $this->password     = $password;
         $this->remember_token = $remember_token;
         $this->created_at   = $created_at;
+        $this->updated_at   = $updated_at;
         $this->first_name   = $fist_name;
         $this->last_name    = $last_name;
     }
@@ -30,7 +33,8 @@ class UserEntity extends EntityFactory
     public static function populate(array $data = [])
     {
         $vars = [
-            'id' => '', 'email' => '', 'password' => '', 'remember_token' => '', 'created_at' => '',
+            'id' => '', 'email' => '', 'password' => '', 'remember_token' => '',
+            'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s'),
             'first_name' => '', 'last_name' => ''
         ];
 
@@ -51,8 +55,10 @@ class UserEntity extends EntityFactory
             'password' => $this->password,
             'remember_token' => $this->remember_token,
             'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
             'first_name' => $this->first_name,
-            'last_name' => $this->last_name
+            'last_name' => $this->last_name,
+            'avatar' => $this->avatar
         ];
     }
 }
