63443b5910e25480608a3831d07a4b4fdf70a936
Gareth Jones <me@gazj.co.uk>
2015-05-03 02:09:42 +0100

    Get managers for team

--- app/Repository/TeamRepository.php
+++ app/Repository/TeamRepository.php
@@ -3,6 +3,8 @@
 namespace Teamnfc\Repository;
 
 
+use Teamnfc\Entity\EntityFactory;
+
 class TeamRepository extends RepositoryManager {
 
     public function getTeamById($teamID)
@@ -17,6 +19,14 @@ class TeamRepository extends RepositoryManager {
 
     public function getManagerForTeam($team)
     {
+        $data =  $this->db->table('team_users')->where('team_id', $team->id)->where('is_manager', 1)->get();
 
+        $entities = [];
+        foreach ($data as $teamUser) {
+            $user = $this->db->table('users')->where('id', $teamUser->user_id)->get()[0];
+            $entities[] = EntityFactory::get('UserEntity', (array)$user);
+        }
+
+        return $entities;
     }
 }
