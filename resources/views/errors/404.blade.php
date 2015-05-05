0cd6011448057d3dfd12249091202512a4cc111a
brunty <openblue555@gmail.com>
2015-05-05 21:59:22 +0100

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('meta-title')
+    404: Page not found | Team NFC
+@endsection
+
+@section('page-title')
+    Four oh four.
+@endsection
+
+@section('content')
+@endsection
